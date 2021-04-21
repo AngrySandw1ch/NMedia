10aa77ab026111fe48ff5ca54640759c4ee8c88a
Artem <artem.verbicky1991@gmail.com>
2021-04-21 19:39:33 +0300

    hometask 2.1: action listener

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -2,10 +2,52 @@ package ru.netology
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.databinding.ActivityMainBinding
+import ru.netology.dto.Post
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Какой-то контент",
+            published = "21 мая в 18:36",
+            likedByMe = false
+        )
+
+        var shareClickCounter = 998
+
+        with(binding) {
+            author.text = post.author
+            content.text = post.content
+            published.text = post.published
+
+            if (post.likedByMe) {
+                like.setImageResource(R.drawable.ic_baseline_favorite_red_24)
+            }
+
+            like.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+                post.likes = 1234
+                if (post.likedByMe) {
+                    post.likes++
+                    likesCounter.text = PostManager.formatNum(post.likes)
+                    like.setImageResource(R.drawable.ic_baseline_favorite_red_24)
+                }
+                else {
+                    post.likes--
+                    likesCounter.text = PostManager.formatNum(post.likes)
+                    like.setImageResource(R.drawable.ic_baseline_favorite_24)
+                }
+            }
+            share.setOnClickListener {
+                shareClickCounter++
+                sharesCounter.text = PostManager.formatNum(shareClickCounter)
+            }
+        }
     }
 }
---
+++ app/src/main/java/ru/netology/PostManager.kt
@@ -0,0 +1,25 @@
+package ru.netology
+
+object PostManager {
+    private const val THOU = 1000;
+    private const val MILL = 1000000;
+
+    fun formatNum(value: Int): String {
+        if (value < THOU) return value.toString();
+        if (value < MILL) return makeDecimal(value, THOU, "k");
+        return makeDecimal(value, MILL, "m");
+    }
+
+    private fun makeDecimal(value: Int, div: Int, suffix: String): String {
+        val vl = value / (div / 10)
+        val whole = vl / 10
+        val tenths = vl % 10
+        if ((tenths == 0) || (whole >= 10)) {
+            return String.format("%d%s", whole, suffix)
+        }
+        return String.format("%d.%d%s", whole, tenths, suffix)
+    }
+
+
+
+}
