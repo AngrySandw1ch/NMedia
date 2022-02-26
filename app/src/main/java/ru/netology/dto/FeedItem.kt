7b63d38b1b4ef509a0d9abae20915445312f984a
Artem Verbicky <artem.verbicky1991@gmail.com>
2022-02-26 18:51:21 +0300

    add FeedItem

--- app/src/main/java/ru/netology/dto/FeedItem.kt
+++ app/src/main/java/ru/netology/dto/FeedItem.kt
@@ -2,9 +2,12 @@ package ru.netology.dto
 
 import ru.netology.enumeration.AttachmentType
 
+sealed class FeedItem {
+    abstract val id: Long
+}
 
 data class Post(
-    val id: Long,
+    override val id: Long,
     val authorId: Long,
     val author: String,
     val content: String,
@@ -15,7 +18,13 @@ data class Post(
     val likedByMe: Boolean,
     val attachment: Attachment? = null,
     val ownedByMe: Boolean = false
-    )
+    ): FeedItem()
+
+data class Ad(
+    override val id: Long,
+    val url: String,
+    val image: String
+): FeedItem()
 
 data class Attachment(
     val url: String,
